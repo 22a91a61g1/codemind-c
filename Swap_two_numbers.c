ab762e602b2371bef7bbc7bdec90142743d7bf32
varun <119839554+22a91a61g1@users.noreply.github.com>
2022-12-19 10:03:20 +0530

    #include<stdio.h> int main() {     int a,b,s,t;     scanf("%d%d",&a,&b);     t=a;     s=b;     printf("%d %d",s,t); }

--- Swap_two_numbers.c
+++ Swap_two_numbers.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-   int a,b,s,t;
-   scanf("%d%d",&a,&b);
-   t=a;
-   s=b;
-   printf("%d
+    int a,b,s,t;
+    scanf("%d%d",&a,&b);
+    t=a;
+    s=b;
+    printf("%d
 %d",s,t);
 }
